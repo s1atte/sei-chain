fb86d88d48f91ba21ceff98ec6b665e5a39e25ac
Brandon Weng <wengbrandon@gmail.com>
2022-08-13 17:00:33 -0700

    bweng profiling changes

--- x/dex/keeper/utils/wasm.go
+++ x/dex/keeper/utils/wasm.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/sei-protocol/sei-chain/x/dex/keeper"
@@ -9,21 +11,27 @@ import (
 
 func CallContractSudo(sdkCtx sdk.Context, k *keeper.Keeper, contractAddr string, msg interface{}) ([]byte, error) {
 	contractAddress, err := sdk.AccAddressFromBech32(contractAddr)
+	fmt.Println("TIME: CallContractSudo Start", time.Now().UTC().UnixMilli())
 	if err != nil {
 		sdkCtx.Logger().Error(err.Error())
 		return []byte{}, err
 	}
+	fmt.Println("TIME: Json Marshal Start", time.Now().UTC().UnixMilli())
 	wasmMsg, err := json.Marshal(msg)
+	fmt.Println("TIME: Json Marshal end", time.Now().UTC().UnixMilli())
 	if err != nil {
 		sdkCtx.Logger().Error(err.Error())
 		return []byte{}, err
 	}
+	fmt.Println("TIME: Sudo Start", time.Now().UTC().UnixMilli())
 	data, err := k.WasmKeeper.Sudo(
 		sdkCtx, contractAddress, wasmMsg,
 	)
+	fmt.Println("TIME: Sudo end", time.Now().UTC().UnixMilli())
 	if err != nil {
 		sdkCtx.Logger().Error(err.Error())
 		return []byte{}, err
 	}
+	fmt.Println("TIME: CallContractSudo end", time.Now().UTC().UnixMilli())
 	return data, nil
 }
