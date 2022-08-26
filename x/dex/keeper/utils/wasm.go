0c867ff8ac352ce2be8848a924a367bdc09b3602
Brandon Weng <wengbrandon@gmail.com>
2022-08-26 01:11:38 -0700

    log statements

--- x/dex/keeper/utils/wasm.go
+++ x/dex/keeper/utils/wasm.go
@@ -2,12 +2,15 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/sei-protocol/sei-chain/x/dex/keeper"
 )
 
 func CallContractSudo(sdkCtx sdk.Context, k *keeper.Keeper, contractAddr string, msg interface{}) ([]byte, error) {
+	fmt.Println(contractAddr)
 	contractAddress, err := sdk.AccAddressFromBech32(contractAddr)
 	if err != nil {
 		sdkCtx.Logger().Error(err.Error())
@@ -18,9 +21,11 @@ func CallContractSudo(sdkCtx sdk.Context, k *keeper.Keeper, contractAddr string,
 		sdkCtx.Logger().Error(err.Error())
 		return []byte{}, err
 	}
+	fmt.Println("TIME Sudo Start", time.Now().UTC().UnixMilli())
 	data, err := k.WasmKeeper.Sudo(
 		sdkCtx, contractAddress, wasmMsg,
 	)
+	fmt.Println("TIME Sudo end", time.Now().UTC().UnixMilli())
 	if err != nil {
 		sdkCtx.Logger().Error(err.Error())
 		return []byte{}, err
