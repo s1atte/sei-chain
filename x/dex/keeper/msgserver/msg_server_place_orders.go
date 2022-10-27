7b27835ce3509d8020e744c0d04633012e6b6621
Eric Zhu <ericzhu77@gmail.com>
2022-10-27 01:46:44 -0400

    replays msg properly

--- x/dex/keeper/msgserver/msg_server_place_orders.go
+++ x/dex/keeper/msgserver/msg_server_place_orders.go
@@ -49,6 +49,7 @@ func (k msgServer) transferFunds(goCtx context.Context, msg *types.MsgPlaceOrder
 }
 
 func (k msgServer) PlaceOrders(goCtx context.Context, msg *types.MsgPlaceOrders) (*types.MsgPlaceOrdersResponse, error) {
+	fmt.Println("~~~ replay place order msg")
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	for _, order := range msg.Orders {
